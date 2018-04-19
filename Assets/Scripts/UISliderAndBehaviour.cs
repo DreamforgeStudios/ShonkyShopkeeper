1c6c9666309c927dade86d45572ce8878240dca3
matthewdavies1212 <33539932+matthewdavies1212@users.noreply.github.com>
2018-04-19 19:06:40 +1000

    Added my prototype scene

---
+++ Assets/Scripts/UISliderAndBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISliderAndBehaviour : MonoBehaviour {
+    public Slider timerSlider;
+    public Image sliderImage;
+    public Text qualityText;
+    private Camera mainCamera;
+	// Use this for initialization
+	void Start () {
+        mainCamera = Camera.main;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timerSlider.minValue = mainCamera.GetComponent<Tracing>().startTime;
+        timerSlider.value = mainCamera.GetComponent<Tracing>().currentTime;
+        timerSlider.maxValue = mainCamera.GetComponent<Tracing>().finishTime;
+        Color sliderColour = Color.Lerp(Color.green, Color.red, timerSlider.value / timerSlider.maxValue);
+        sliderImage.color = sliderColour;
+        QualityText();
+    }
+
+    public void QualityText() {
+        qualityText.text = "Quality = " + mainCamera.GetComponent<Tracing>().itemQuality;
+    }
+}
