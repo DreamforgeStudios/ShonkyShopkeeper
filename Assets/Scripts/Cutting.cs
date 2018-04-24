1c420a86bad999dfb39a409c9b7c2759c0608e93
Michael Palmos <toqoz@hotmail.com>
2018-04-24 22:54:16 +1000

    cutting minigame scoring / accuracy is done.  just need to make it work in a real scenario and add some flare / feedback.

--- Assets/Scripts/Cutting.cs
+++ Assets/Scripts/Cutting.cs
@@ -2,18 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// INFO: Don't use InputManager (which will be event based) in this class.  It needs more precision and more details.
 public class Cutting : MonoBehaviour {
 	public Vector3[] cutOrigins;
 	public Vector3[] cutVectors;
+	public float[] cutTimes;
+
+	// The maximum distance that the cut origin can be from the intended origin to be considered a fail.
 	public float maximumDistance;
 	// The amount that the distance should affect the final result.
 	public float impactDistance;
+
+	// The maximum difference that the closeness can be to be considered a fail.
 	public float maximumCloseness;
-	// The amount that the closeness should affect the final result.
 	public float impactCloseness;
+
 	public float maximumLength;
-	// The amount that the length should affect the final result.
 	public float impactLength;
+
+	public float maximumTimeDiff;
 
 	// (?) Allow these vectors to be nullable, so we can reset them more conveniently later.
 	// This also means that we have to use .Value to get the value of these vectors.
@@ -27,13 +34,23 @@ public class Cutting : MonoBehaviour {
 	private float oCloseness;
 	private float vCloseness;
 	private float lCloseness;
+	public GameObject debugObject;
 
 	// Use this for initialization
 	void Start () {
-		// Make these more convenient for input/use.
-		//impactDistance = 1f / impactDistance;
-		//impactCloseness = 1f / impactCloseness;
-		//impactLength = 1f / impactLength;
+		currentIndex = 0;
+		// DEBUG.
+		for (int i = 0; i < cutOrigins.Length; i++) {
+			// Place objects at start and end positions.
+			Instantiate(debugObject, cutOrigins[i], Quaternion.identity);
+			//Instantiate(debugObject, cutOrigins[i] + cutVectors[i], Quaternion.identity);
+			Vector3 start = cutOrigins[i];
+			Vector3 end = start + cutVectors[i];
+			if (i == currentIndex)
+				Debug.DrawLine(start, end, Color.red, 200);
+			else
+				Debug.DrawLine(start, end, Color.blue, 200);
+		}
 	}
 	
 	// Update is called once per frame
@@ -51,6 +68,7 @@ public class Cutting : MonoBehaviour {
 		#endif
 	}
 
+	// TODO, program for touch events.
 	private void ProcessTouch() {
 		// Loop through all current touch events.
 		foreach (Touch touch in Input.touches) {
@@ -64,14 +82,18 @@ public class Cutting : MonoBehaviour {
 	}
 
 	private void ProcessMouse() {
+		// TODO: record time.
 		if (Input.GetMouseButtonDown(0)) {
-			Debug.Log("Clicked the screen at position: " + Input.mousePosition);
-			touchOrigin = Input.mousePosition;
+			//Debug.Log("Clicked the screen at position: " + Input.mousePosition);
+			touchOrigin = ConvertToWorldPoint(Input.mousePosition);
 		}
 
 		if (Input.GetMouseButtonUp(0)) {
 			Vector2 mousePos = Input.mousePosition;
-			touchVector = touchOrigin - mousePos;
+			touchVector = ConvertToWorldPoint(mousePos) - touchOrigin;
+			float close = CalculateCloseness(touchOrigin.Value, touchVector.Value);
+			DrawDebugLine(touchOrigin.Value);
+			//Debug.Log(close);
 
 			// Reset the origin.
 			touchOrigin = null;
@@ -94,7 +116,22 @@ public class Cutting : MonoBehaviour {
 		}
 	}
 
+	private void DrawDebugLine(Vector3 origin) {
+		Vector3 end = ConvertToWorldPoint(Input.mousePosition);
+		Debug.DrawLine(origin, end, Color.yellow, 2);
+	}
+
+	// TODO: this will eventually go in the input manager.
+	private Vector3 ConvertToWorldPoint(Vector3 screenPoint) {
+		// If this is changed from 10, it fucks up.
+		// I think that 10 indicates 10 units FROM the camera, not 10 in the scene.
+		screenPoint.z = 10;
+		return Camera.main.ScreenToWorldPoint(screenPoint);
+	}
+
 	private float CalculateCloseness(Vector3 origin, Vector3 vec) {
+		//origin = ConvertToWorldPoint(origin);
+
 		// 0 = close, 1 = far.
 		float originCloseness = 0f;
 		float vectorCloseness = 0f;
@@ -111,7 +148,8 @@ public class Cutting : MonoBehaviour {
 		// Normalize so that distance doesn't affect the dot product.
 		Vector3 vn = Vector3.Normalize(vec);
 		Vector3 cn = Vector3.Normalize(cutVectors[currentIndex]);
-		float vSimilarity = Vector3.Dot(vn, cn);
+		float vSimilarity = 1-Vector3.Dot(vn, cn);
+		Debug.Log(vSimilarity);
 		vectorCloseness = Mathf.InverseLerp(0, maximumCloseness, vSimilarity);
 		this.vCloseness = vectorCloseness;
 
