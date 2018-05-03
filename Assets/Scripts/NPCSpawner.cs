fac1a466a1a351abaecf2b610775d1c797a120a3
Michael Palmos <toqoz@hotmail.com>
2018-05-03 19:02:58 +1000

    NPC spawn and walk behaviour.

---
+++ Assets/Scripts/NPCSpawner.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSpawner : MonoBehaviour {
+	// The points where NPCS should spawn from.
+	public Vector3[] spawns;
+	// The direction that the NPC should walk in.
+	public int[] spawnDirections;
+	// Potential NPC gameobjects to spawn.
+	public GameObject[] potentialSpawns;
+	// How fast should each NPC move?
+	public float walkSpeed;
+
+	// A random scale will be chosen between these two values (inclusive - exclusive).
+	public float scaleMin;
+	public float scaleMax;
+
+	// Time between spawns.
+	// Alter this from another class to increase / decrease spawn rate.
+	public float spawnInterval;
+	private float timer;
+
+	// For drawing gizmos.
+	public bool debug = true;
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		// Instantiate an NPC and set them walking.
+		if (timer > spawnInterval) {
+			GameObject npcToSpawn = potentialSpawns[Random.Range(0, potentialSpawns.Length)];
+			int positionToSpawn = Random.Range(0, spawns.Length);
+			GameObject clone = Instantiate(npcToSpawn, spawns[positionToSpawn], Quaternion.identity);
+			NPCWalker walker = clone.GetComponent<NPCWalker>();
+
+			walker.SetWalkDirection(spawnDirections[positionToSpawn]);
+			walker.SetWalkSpeed(walkSpeed);
+			walker.SetScale(Random.Range(scaleMin, scaleMax));
+
+			timer = 0f;
+		}
+	}
+
+	public void SetSpawnInterval(float interval) {
+		this.spawnInterval = interval;
+	}
+
+	private void OnDrawGizmos() {
+		if (!debug) {
+			return;
+		}
+
+		for (int i = 0; i < spawns.Length; i++) {
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawWireCube(spawns[i], Vector3.one);
+			Gizmos.color = Color.blue;
+			Gizmos.DrawLine(spawns[i], spawns[i] + new Vector3(spawnDirections[i] * 5, 0, 0));
+		}
+	}
+}
---
+++ Assets/Scripts/NPCWalker.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCWalker : MonoBehaviour {
+	public int walkDirection;
+	// TODO: random walk speed variance?
+	public float walkSpeed;
+
+	private bool enteredScreen = false;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("TestAndDestroy", 2f, 2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3(walkDirection * walkSpeed * Time.deltaTime, 0, 0);
+	}
+
+	// Check if the object is on the screen.  If not, destroy.
+	private void TestAndDestroy() {
+		bool onScreen = TestOnScreen();
+		if (onScreen && !enteredScreen) {
+			enteredScreen = true;
+		} else if (!onScreen && enteredScreen) {
+			Destroy(this.gameObject);
+		}
+	}
+
+	private bool TestOnScreen() {
+		Vector3 screenPoint = Camera.main.WorldToViewportPoint(transform.position);
+		return screenPoint.z > 0 && screenPoint.x > 0 &&
+			   screenPoint.x < 1 && screenPoint.y > 0 &&
+			   screenPoint.y < 1;
+	}
+
+	public void SetWalkDirection(int walkDirection) {
+		this.walkDirection = walkDirection;
+	}
+
+	public void SetWalkSpeed(float walkSpeed) {
+		this.walkSpeed = walkSpeed;
+	}
+
+	public void SetScale(float scale) {
+		transform.localScale = new Vector3(scale * transform.localScale.x,
+										   scale * transform.localScale.y,
+										   transform.localScale.z);
+	}
+
+	// Later...???
+	/*
+	Raycast to this object and run this function.
+	public void LoadRealVersion() {
+		??
+	}
+	*/
+}
