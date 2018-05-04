42859c0c2b52398d0aaf7b21f342b8e5c598da63
Michael Palmos <toqoz@hotmail.com>
2018-05-04 14:01:19 +1000

    Shonky inspector / information.

--- Assets/Scripts/Shonky.cs
+++ Assets/Scripts/Shonky.cs
@@ -7,4 +7,14 @@ public class Shonky : Item {
     public Quality.QualityGrade quality;
     public GemType type;
     public ItemType itemType = ItemType.Shonky;
+
+    // Return a string to be used in the UI.
+    // TODO: need a way to get item color.
+    public string GetInfoString() {
+        Color col = Quality.GradeToColor(quality);
+        string colString = "<color=#" + ColorUtility.ToHtmlStringRGBA(col) + ">";
+        string terminate = "</color>";
+        return string.Format("Quality: {0}{1}{2}" + System.Environment.NewLine +
+					         "Type: {3}{4}{5}", colString, quality, terminate, colString, type, terminate);
+    }
 }
