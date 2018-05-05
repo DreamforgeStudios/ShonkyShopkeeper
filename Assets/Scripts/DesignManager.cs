cee17f9b9ed7da473b98ab99768b50b144a718be
Michael Palmos <toqoz@hotmail.com>
2018-05-05 18:02:45 +1000

    Design purchase screen is made and working, but designs can't be purchased yet.

---
+++ Assets/Scripts/DesignManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DesignManager : MonoBehaviour {
+	public TextMeshProUGUI textItem;
+	public GameObject golumContainer;
+
+	public List<GameObject> shonkyList;
+	public int currentIndex = 0;
+
+	public void IncrementIndex() {
+		// Increment if not at max.
+		currentIndex = (currentIndex >= shonkyList.Count-1) ? 0 : currentIndex + 1;
+		UpdateDesign();
+	}
+
+	public void DecrementIndex() {
+		// Decrement if not at min.
+		currentIndex = (currentIndex <= 0) ? shonkyList.Count-1 : currentIndex - 1;
+		UpdateDesign();
+	}
+
+	public GameObject GetCurrentDesign() {
+		return shonkyList[currentIndex];
+	}
+
+	public void UpdateDesign() {
+		if (golumContainer.transform.childCount > 0) {
+			Destroy(golumContainer.transform.GetChild(0).gameObject);
+		}
+
+		Instantiate(shonkyList[currentIndex], golumContainer.transform);
+	}
+}
