c2f20b581f525ef66b32c83c6c83c6817c3f6ae6
matthewdavies1212 <33539932+matthewdavies1212@users.noreply.github.com>
2018-05-18 12:25:50 +1000

    Redesign tracing minigame to use colliders and a sphere. Added included art to the tracing minigame

---
+++ Assets/Scripts/TracingColliding.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TracingColliding : MonoBehaviour {
+    public int counter = 0;
+    public GameObject colliderHit;
+    public Image holder;
+    public Sprite bad;
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "TracingCollider") {
+            counter++;
+            Instantiate(colliderHit, this.transform.position,this.transform.rotation);
+                holder.enabled = true;
+                holder.sprite = bad;
+           
+        }
+    }
+
+    private void OnCollisionStay(Collision collision) {
+        if (collision.gameObject.tag == "TracingCollider") {
+            counter++;
+            
+        }
+    }
+}
