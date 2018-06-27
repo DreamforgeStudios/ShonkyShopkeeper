57843974081ab56aac38407f7d43a0dbe426e57d
Toqoz <toqoz@hotmail.com>
2018-06-27 12:50:45 +1000

    same small cleanup

--- Assets/Scripts/SaveManager.cs
+++ Assets/Scripts/SaveManager.cs
@@ -1,28 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 [CreateAssetMenu]
-public class SaveManager : ScriptableObject {
-	public static void LoadOrInitializeInventory(Inventory inventoryTemplate) {
-		// Saving and loading.
-		if (System.IO.File.Exists(System.IO.Path.Combine(Application.persistentDataPath, "inventory.json"))) {
-			Debug.Log("Found file inventory.json, loading inventory.");
-			Inventory.LoadFromJSON(System.IO.Path.Combine(Application.persistentDataPath, "inventory.json"));
-		} else {
+public class SaveManager {
+    public static void LoadOrInitializeInventory(Inventory inventoryTemplate) {
+        // Saving and loading.
+        if (File.Exists(Path.Combine(Application.persistentDataPath, "inventory.json"))) {
+            Debug.Log("Found file inventory.json, loading inventory.");
+            Inventory.LoadFromJSON(Path.Combine(Application.persistentDataPath, "inventory.json"));
+        } else {
             Debug.Log("Couldn't find inventory.json, loading from template.");
-			Inventory.InitializeFromDefault(inventoryTemplate);
-		}
-	}
+            Inventory.InitializeFromDefault(inventoryTemplate);
+        }
+    }
     public static void SaveInventory() {
-        Inventory.Instance.SaveToJSON(System.IO.Path.Combine(Application.persistentDataPath, "inventory.json"));
+        Inventory.Instance.SaveToJSON(Path.Combine(Application.persistentDataPath, "inventory.json"));
     }
 
     public static void LoadOrInitializeShonkyInventory(ShonkyInventory shonkyInventoryTemplate) {
         // Saving and loading.
-        if (System.IO.File.Exists(System.IO.Path.Combine(Application.persistentDataPath, "shonkyinventory.json"))) {
+        if (File.Exists(Path.Combine(Application.persistentDataPath, "shonkyinventory.json"))) {
             Debug.Log("Found file shonkyinventory.json, loading shonky inventory.");
-            ShonkyInventory.LoadFromJSON(System.IO.Path.Combine(Application.persistentDataPath, "shonkyinventory.json"));
+            ShonkyInventory.LoadFromJSON(Path.Combine(Application.persistentDataPath, "shonkyinventory.json"));
         }
         else {
             Debug.Log("Couldn't find shonkyinventory.json, loading from template.");
@@ -31,17 +32,17 @@ public class SaveManager : ScriptableObject {
     }
 
     public static void SaveShonkyInventory() {
-        ShonkyInventory.Instance.SaveToJSON(System.IO.Path.Combine(Application.persistentDataPath, "shonkyinventory.json"));
+        ShonkyInventory.Instance.SaveToJSON(Path.Combine(Application.persistentDataPath, "shonkyinventory.json"));
     }
 
 
-	// Load from default.
-	public static void LoadFromTemplate(Inventory template) {
-		Inventory.InitializeFromDefault(template);
-	}
+    // Load from default.
+    public static void LoadFromTemplate(Inventory template) {
+        Inventory.InitializeFromDefault(template);
+    }
 
-	// Load from default.
-	public static void LoadFromShonkyTemplate(ShonkyInventory template) {
-		ShonkyInventory.InitializeFromDefault(template);
-	}
+    // Load from default.
+    public static void LoadFromShonkyTemplate(ShonkyInventory template) {
+        ShonkyInventory.InitializeFromDefault(template);
+    }
 }
