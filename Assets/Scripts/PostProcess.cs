a33b11f52af4dc1b6d144994519a50553516223e
Toqoz <toqoz@hotmail.com>
2018-07-05 21:14:46 +1000

    implement return or retry systems properly (no retry count yet though), and refactor some code

---
+++ Assets/Scripts/PostProcess.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PostProcess : MonoBehaviour {
+
+	public Shader shader;
+	public Texture vignette;
+	public float intensity;
+
+	private Material mat;
+	
+	// Use this for initialization
+	void Start () {
+		mat = new Material(shader);
+		mat.SetTexture("_Vignette", vignette);
+	}
+
+	private void Update() {
+		mat.SetFloat("_Intensity", intensity);
+	}
+
+	private void OnRenderImage(RenderTexture src, RenderTexture dest) {
+		Graphics.Blit(src, dest, mat);
+	}
+}
