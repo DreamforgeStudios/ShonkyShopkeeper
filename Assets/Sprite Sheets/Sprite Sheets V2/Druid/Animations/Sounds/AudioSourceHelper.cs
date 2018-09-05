7c2d16f1e38b69646381391d60c46981cfd81a59
Kyle <kyleanderson891@gmail.com>
2018-09-05 18:44:36 +1000

    Sprite Sheet V2 Repo Fix

---
+++ Assets/Sprite Sheets/Sprite Sheets V2/Druid/Animations/Sounds/AudioSourceHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioSourceHelper : MonoBehaviour {
+    public AudioSource source;
+
+    private void OnDestroy() {
+        SFX.RemoveSource(this);
+    }
+}
---
+++ Assets/Sprite Sheets/Sprite Sheets V2/Druid/Animations/Sounds/SFX.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public static class SFX {
+    private static SoundDatabase
+        soundDB = Object.Instantiate((SoundDatabase) Resources.Load("SFXDatabase"));
+    
+    public static void Play(string sound, float volume = 1, float pitch = 1, float delay = 0, bool looping = false, float playaAt = 0) {
+        var clip = soundDB.GetClip(sound);
+
+        var source = soundDB.AudioSourceInstance;
+        source.clip = clip;
+        source.volume = volume;
+        source.pitch = pitch;
+        source.time = playaAt;
+        source.loop = looping;
+
+        source.PlayDelayed(delay);
+    }
+
+    public static void StopAll() {
+        soundDB.StopAll();
+    }
+
+    public static void StopSpecific(string sound) {
+        soundDB.StopSpecific(sound);
+    }
+
+    public static void RemoveSource(AudioSourceHelper source) {
+        soundDB.RemoveSource(source);
+    }
+}
