c974eaaa5a7784cc0838fba91983b29a44302ca1
Toqoz <toqoz@hotmail.com>
2018-10-18 20:08:53 +1000

    TODO list updated with new artwork and can unravel / ravel on click.

---
+++ Assets/Scripts/Utility/ScrollToggle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ScrollToggle : MonoBehaviour, IPointerClickHandler {
+	public Sprite OpenSprite, ClosedSprite;
+	public Image Image;
+
+	public GameObject Heading, List;
+
+	private bool open = true;
+
+	private void OpenClose() {
+		Debug.Log("Click");
+		
+		if (open) {
+			open = false;
+			Image.sprite = ClosedSprite;
+			Heading.SetActive(false);
+			List.SetActive(false);
+		} else {
+			open = true;
+			Image.sprite = OpenSprite;
+			Heading.SetActive(true);
+			List.SetActive(true);
+		}
+	}
+
+	public void OnPointerClick(PointerEventData eventData) {
+		OpenClose();
+	}
+}
